1824c81f4e3354520a8609db4e7a2c20b2e4dc06
Stevi Hall <career.sh.work@gmail.com>
2020-02-03 20:32:13 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,11 @@
 def using_until
-  levitation_force = 6
-  #your code here
-    
 end
-
+def levitation_force
+end
+  levitation_force = 5
+  until levitation_force == 10
+   
+puts "Wingardium Leviosa" 
+levitation_force += 1
+end
+end
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,15 @@
 def using_while
-  levitation_force = 6
-	
-  #your code here
 end
+levitation_force = 6
+
+while levitation_force = 10
+levitaion_force += 1
+puts "Wingardium Leviosa"
+#levitaion_force += 1
+end
+
+
+
+
 
 
